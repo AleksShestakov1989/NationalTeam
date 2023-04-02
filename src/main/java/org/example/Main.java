95940eff938f32191c7f5bc92a225e6a8b7b3174
Aleks <shestakov.aleksey8989@gmail.com>
2023-04-02 13:52:18 +0300

    new commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(national_team(regionalTeams)));
+        System.out.println("Bay");
     }
 
     public static int[] merge(int[] team, int[] regionalTeam) {
